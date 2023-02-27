310e476eb51974e1de23e9da8cf30f7bd380ae37
lakdav <ladav2018@gmail.com>
2023-02-27 09:38:30 +0300

    first commit

---
+++ src/guessing-game.js
@@ -0,0 +1,23 @@
+class GuessingGame {
+	constructor() {}
+
+	setRange(min, max) {
+		this.min = min;
+		this.max = max;
+	}
+
+	guess() {
+		this.candidate = Math.ceil((this.min + this.max) / 2);
+		return this.candidate;
+	}
+
+	lower() {
+		this.max = this.candidate;
+	}
+
+	greater() {
+		this.min = this.candidate;
+	}
+}
+
+module.exports = GuessingGame;
